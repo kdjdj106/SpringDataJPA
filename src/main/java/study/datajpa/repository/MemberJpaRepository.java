b75cb9024d7c00e549fa5c69729ba2d5ea8e5eac
kdjdj106 <kdjdj106@naver.com>
2024-01-27 19:42:07 +0900

    init

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -7,6 +7,9 @@ import jakarta.persistence.PreUpdate;
 import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public class MemberJpaRepository {
 
@@ -20,5 +23,21 @@ public class MemberJpaRepository {
 
     public Member find(Long id){
         return em.find(Member.class, id);
+    }
+    public Optional<Member> findById(Long id){
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+    public long count(){
+        return em.createQuery("select count(m) FROM Member m", Long.class)
+                .getSingleResult();
+    }
+    public void delete(Member member){
+        em.remove(member);
+    }
+    public List<Member> findAll(){
+        List<Member> result = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+        return result;
     }
 }
--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -9,6 +9,8 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 
@@ -32,5 +34,28 @@ class MemberJpaRepositoryTest {
         assertThat(findMember).isEqualTo(member);
 
 
+    }
+
+    @DisplayName("")
+    @Test
+    public void basicCRUD() {
+        //given
+        Member member = new Member("member1");
+        Member member2 = new Member("member2");
+        memberJpaRepository.save(member);
+        memberJpaRepository.save(member2);
+
+        Member findmember1 = memberJpaRepository.findById(member.getId()).get();
+        Member findmember2 = memberJpaRepository.findById(member2.getId()).get();
+
+        List<Member> all = memberJpaRepository.findAll();
+        long count = memberJpaRepository.count();
+        //when
+        //then
+        assertThat(findmember1).isEqualTo(member);
+        assertThat(findmember2).isEqualTo(member2);
+
+        assertThat(all.size()).isEqualTo(2);
+        assertThat(count).isEqualTo(2);
     }
 }
